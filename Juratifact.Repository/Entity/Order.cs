bc28c322fe87665d90d09d3275c9a14229fb98dd
Thien Hao <93728241+thienhao05@users.noreply.github.com>
2026-05-05 15:22:21 +0700

    feat(order): add cancelReason field

--- Juratifact.Repository/Entity/Order.cs
+++ Juratifact.Repository/Entity/Order.cs
@@ -14,6 +14,7 @@ public class Order: BaseEntity<Guid>,IAuditableEntity
     public DateTimeOffset? PickupAt { get; set; }
     public DateTimeOffset? DeliveryAt { get; set; }
     public string? EvidenceUrl  { get; set; }
+    public string? CancelReason { get; set; }
     
     public OrderStatus Status { get; set; }
     public PaymentStatus PaymentStatus { get; set; }
---
+++ Juratifact.Repository/Migrations/20260505081914_updateCancelReasonOrder.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Juratifact.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateCancelReasonOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CancelReason",
+                table: "Orders",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CancelReason",
+                table: "Orders");
+        }
+    }
+}
