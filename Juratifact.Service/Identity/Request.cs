a924d24f5686b643a88e15ddc162549303911edd
Thien Hao <93728241+thienhao05@users.noreply.github.com>
2026-05-11 15:47:25 +0700

    fix(auth): hide email and password from login URL

---
+++ Juratifact.Service/Identity/Request.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Juratifact.Service.Identity;
+
+public class Request
+{
+    public class LoginRequest
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
