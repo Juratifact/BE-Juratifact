69984e16373a822814dbbec8098c06452c727926
tranminhthien-tech <tranminhthien188@gmail.com>
2026-05-15 12:28:13 +0700

    update order

--- Juratifact.Service/Order/IOrderService.cs
+++ Juratifact.Service/Order/IOrderService.cs
@@ -4,11 +4,11 @@ public interface IOrderService
 {
     public Task<Response.CreateOrderResponse> CreateOrderProduct(Request.CheckoutRequest request);
     public Task<Response.GetOrderStatusResponse> GetStatusOrder(Guid id);
-    public Task<List<Response.GetAllOrderResponse>>  GetAllOrders();
+    public Task<Base.Response.PageResult<Response.GetAllOrderResponse>>  GetAllOrders(int pageSize, int pageIndex);
     public Task<string> ConfirmReceipt(Guid orderId);
     public Task<string> CancelOrder(Guid orderId, Request.CancelOrderRequest request);
     public Task<string> CancelCheckout(Guid orderId);
-    public Task<List<Response.GetMyOrderResponse>> GetMyOrder();
+    public Task<Base.Response.PageResult<Response.GetMyOrderResponse>> GetMyOrder(int pageSize, int pageIndex);
     public Task<Response.ProductListResponse> GetProductbyOrderId(Guid orderId, Guid productId);
     public Task<string> UpdateShippingAddress(Guid orderId, Request.UpdateShippingAddressRequest request);
 }
